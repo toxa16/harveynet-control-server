b724247efda57bff200c218820200d6b308a4631
toxa16 <toxa16@gmail.com>
2019-11-29 19:34:17 +0200

    machine list feature stub

--- src/wss.js
+++ src/wss.js
@@ -4,11 +4,32 @@ const App = require('./app');
 
 const wss = new WebSocket.Server({ noServer: true });
 
+const machinesFixture = [
+  {
+    id: 'machine2',
+    isOnline: false,
+  },
+  {
+    id: 'machine1',
+    isOnline: true,
+  },
+];
+
 const app = new App();
 //wss.on('connection', app.handleSocketConnect);
 wss.on('connection', (ws, req) => {
   const { remotePort } = req.socket;
   console.log(`Client ${remotePort} connected.`);
+
+
+  const action = {
+    type: 'MACHINE_LIST_UPDATE',
+    payload: {
+      machines: machinesFixture,
+    },
+  };
+  ws.send(JSON.stringify(action));
+
 
   ws.on('close', () => {
     console.log(`${remotePort} disconnected.`);
